58c3388c16be878026c5938212b4284088e973b2
Venkat Nithin <venkatnithinm@gmail.com>
2024-01-17 10:44:46 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,58 +1,78 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
-];
-
-// initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-
+    'Spooderman',
+    'Shield Boi',
+    'Wonderwoman',
+    'Ballhead',
+    'Visonite',
+    'Clap',
+    'Wasp',
+    'Mask',
+    'Feline',
+    'Sergeant',
+    'Catwoman',
+    'Fish',
+    'Bulk',
+    'Ninja',
+    'Black Boi',
+    'Wolverine',
+    'Thor',
+    'Slayer',
+    'Vader',
+    'Slingo',
+  ];
+  
+  const initPlayers = (players) => {
+    const detailedPlayers = [];
+    players.forEach((a, b) => {
+      detailedPlayers.push({
+        name: a,
+        strength: getRandomStrength(),
+        image: 'images/super-' + (b + 1) + '.png',
+        type: b % 2 == 0 ? 'hero' : 'villain',
+        id: b + 1,
+      });
+    });
     return detailedPlayers;
-}
-
-// getting random strength
+  };
+  
 const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
-}
-
-const buildPlayers = (players, type) => {
-    let fragment = '';
-
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
-
-    return fragment;
-}
-// Display players in HTML
-const viewPlayers = (players) => {
-
+    return Math.floor(Math.random() * 100) + 1;
+  };
+  
+  const view = (xyz) => {
+    const player = document.createElement('div');
+    player.classList.add('player');
+    const image = document.createElement('img');
+    image.setAttribute('src', xyz.image);
+    image.setAttribute('alt', '');
+    const name = document.createElement('div');
+    name.className = 'name';
+    name.textContent = xyz.name;
+    const strength = document.createElement('div');
+    strength.textContent = xyz.strength;
+    strength.className = 'strength';
+    player.append(image, name, strength);
+    return player;
+  };
+  
+  const buildPlayers = (players, type) => {
+    const fragment = document.createElement('div');
+    players
+      .filter((player) => player.type == type)
+      .forEach((player) => fragment.append(view(player)));
+    return fragment.innerHTML;
+  
+    
+  };
+  const viewPlayers = (players) => {
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-
-}
-
-window.onload = () => {
+    document.getElementById('villains').innerHTML = buildPlayers(
+      players,
+      'villain'
+    );
+  };
+  
+  window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
-}
+  };
+  
